3f2b00b5cc01ceae7394d10bc720ef2d633fbd43
Moercell <marcel.pahnke@gmail.com>
2023-10-01 07:28:18 +0200

    try using cord

--- src/api/data.js
+++ src/api/data.js
@@ -1,13 +1,25 @@
 // pages/api/gameData.js
-export default function handler(req, res) {
-    if (req.method === 'POST') {
-      const { itemData } = req.body;
-  
-      // You can process the itemData here as needed
-  
-      res.status(200).json({ receivedData: 'all good' });
-    } else {
-      res.status(405).json({ error: 'Method Not Allowed' });
-    }
+import Cors from 'cors';
+
+// Initialize the CORS middleware
+const cors = Cors({
+  methods: ['POST'], // Allow only POST requests
+});
+
+// Add the CORS middleware to your route
+export default async function handler(req, res) {
+  // Use the `cors` middleware
+  await cors(req, res);
+
+  if (req.method === 'POST') {
+    // Retrieve the data from the request body
+    const { itemData } = req.body;
+
+    // You can process the itemData here as needed
+
+    // Send a response (for example, echoing the data)
+    res.status(200).json({ receivedData: itemData });
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
   }
-  
+}
